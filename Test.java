181ff28c792973cc3b0d9219e74cb1d9c25f0533
pepliseb <eman@onet.eu>
2017-08-07 22:09:31 +0200

    Porjekt OOP - Pojazd 2

--- Test.java
+++ Test.java
@@ -15,7 +15,7 @@ public class Test {
         PojazdSilnikowy pojazdOsoA = new SamochodOsobowy(5,300,32,3,3,Color.BLUE,3,3, true, "elektryczny");
         PojazdSilnikowy pojazdOsoB = new SamochodOsobowy(7,350,3,3,3,Color.WHITE,3,3,false,"diesel");
         PojazdSilnikowy pojazdOsoC = new SamochodOsobowy(4,250,3,3,3,Color.BLUE,3,3,false,"benzyna");
-        PojazdSilnikowy pojazdOsoD = new SamochodOsobowy(5,700,3,3,3,Color.BLUE,3,3, true,"elektryczny");
+        PojazdSilnikowy pojazdOsoD = new SamochodOsobowy(50,700,3,3,3,Color.BLUE,3,3, true,"elektryczny");
 
 
         PojazdSilnikowy pojazdSilnikowy = new SamochodDostawczy(5,3000,3,4,5,Color.RED,3,4,true, 400,"benzyna");
@@ -46,7 +46,26 @@ public class Test {
         System.out.println(pojazdCieD.getPassenger());
         System.out.println(pojazdOsoD.getTypeEngine());
         System.out.println(pojazdOsoA.getFuelcap());
+
+
+        Pojazd[] arrayOfPojazd = {pojazdCieA,pojazdCieB,pojazdSilnikowy};
+
+        for (Pojazd pojazd : arrayOfPojazd) {
+            System.out.println("Liczba pasażerów  " + pojazd.getPassenger() + " Lista pojazdów " + pojazd.getClass() + pojazd.getKolor() + pojazd.getDist() );
+        }
+
+
+
+        SamochodOsobowy[] arrayOfSamochodOsobowy = {pojazdOsoD};
+
+        for (SamochodOsobowy samochodOsobowy : arrayOfSamochodOsobowy) {
+            System.out.println(samochodOsobowy.isCzyHybryda() + samochodOsobowy.getTypeEngine());
+        }
+
+
         
+
+
 //        System.out.println(pojazdSilnikowy.setPassenger(4));
     }
 
